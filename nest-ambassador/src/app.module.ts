b6ec63f688381d67e492bf7f748091d78a2ddf77
maniizu3110 <maniizu3110@gmail.com>
2022-01-16 15:55:18 +0900

    nestjsで環境変数から読み出すように変更

--- nest-ambassador/src/app.module.ts
+++ nest-ambassador/src/app.module.ts
@@ -1,41 +1,44 @@
-import {Module} from '@nestjs/common';
-import {AppController} from './app.controller';
-import {AppService} from './app.service';
-import {TypeOrmModule} from "@nestjs/typeorm";
-import {UserModule} from './user/user.module';
-import {AuthModule} from './auth/auth.module';
-import {ProductModule} from './product/product.module';
-import {OrderModule} from './order/order.module';
-import {LinkModule} from './link/link.module';
-import {SharedModule} from './shared/shared.module';
-import {EventEmitterModule} from "@nestjs/event-emitter";
-import {ConfigModule} from "@nestjs/config";
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { ProductModule } from './product/product.module';
+import { OrderModule } from './order/order.module';
+import { LinkModule } from './link/link.module';
+import { SharedModule } from './shared/shared.module';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import { ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-    imports: [
-        TypeOrmModule.forRoot({
-            type: 'mysql',
-            host: 'db',
-            port: 3306,
-            username: 'root',
-            password: 'root',
-            database: 'ambassador',
-            autoLoadEntities: true,
-            synchronize: true,
-        }),
-        EventEmitterModule.forRoot(),
-        ConfigModule.forRoot({
-            isGlobal: true
-        }),
-        UserModule,
-        AuthModule,
-        ProductModule,
-        OrderModule,
-        LinkModule,
-        SharedModule,
-    ],
-    controllers: [AppController],
-    providers: [AppService],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('MYSQL_HOST'),
+        port: configService.get('MYSQL_PORT'),
+        username: configService.get('MYSQL_USERNAME'),
+        password: configService.get('MYSQL_PASSWORD'),
+        database: configService.get('MYSQL_DATABASE'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+    EventEmitterModule.forRoot(),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    UserModule,
+    AuthModule,
+    ProductModule,
+    OrderModule,
+    LinkModule,
+    SharedModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
-export class AppModule {
-}
+export class AppModule {}
