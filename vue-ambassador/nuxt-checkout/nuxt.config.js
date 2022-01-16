51a428fcbdae35058b4f16f302d8d450199a325c
maniizu3110 <maniizu3110@gmail.com>
2022-01-16 14:19:05 +0900

    create env

--- vue-ambassador/nuxt-checkout/nuxt.config.js
+++ vue-ambassador/nuxt-checkout/nuxt.config.js
@@ -2,20 +2,24 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   ssr: true,
   head: {
-    title: 'nuxt-checkout',
+    title: "nuxt-checkout",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en"
     },
     meta: [
-      {charset: 'utf-8'},
-      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
-      {hid: 'description', name: 'description', content: ''}
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" }
     ],
     link: [
-      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
       {
-        rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css',
-        integrity: 'sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6', crossOrigin: 'anonymous'
+        rel: "stylesheet",
+        href:
+          "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css",
+        integrity:
+          "sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6",
+        crossOrigin: "anonymous"
       }
     ]
   },
@@ -32,22 +36,29 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build',
+    "@nuxt/typescript-build"
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
+    "@nuxtjs/axios",
 
-    ['nuxt-stripe-module', {
-      publishableKey: 'pk_test_51H0wSsFHUJ5mamKON9UhPL8Rws5mF3p5aTqKK9kh2Rvk0DnQcpSLYujzQoWqTyp02tkWjYB8o9YRKSFWThARxyT100uqDDnoVy',
-    }],
+    [
+      "nuxt-stripe-module",
+      {
+        publishableKey:
+          "pk_test_51H0wSsFHUJ5mamKON9UhPL8Rws5mF3p5aTqKK9kh2Rvk0DnQcpSLYujzQoWqTyp02tkWjYB8o9YRKSFWThARxyT100uqDDnoVy"
+      }
+    ]
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    baseURL: 'http://localhost:8000/api/checkout/'
+    baseURL:
+      process.env.NODE_ENV === prod
+        ? "https://crame.link/api/checkout"
+        : "http://localhost:8000/api/checkout/"
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
@@ -55,4 +66,4 @@ export default {
   server: {
     port: 5000
   }
-}
+};
--- vue-ambassador/vue-ambassador/src/main.ts
+++ vue-ambassador/vue-ambassador/src/main.ts
@@ -4,7 +4,10 @@ import router from './router'
 import store from './store'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://localhost:8000/api/ambassador/';
+axios.defaults.baseURL =
+  process.env.NODE_ENV === "prod"
+    ? "https://crame.link/api/ambassador"
+    : "http://localhost:8000/api/ambassador/";
 axios.defaults.withCredentials = true;
 
 createApp(App).use(store).use(router).mount('#app')
