6e0da5dfa0131529c66cc6846fba671e00b9591f
maniizu3110 <maniizu3110@gmail.com>
2022-01-16 14:02:55 +0900

    add dockerfile+env

--- vue-ambassador/vue-admin/src/main.ts
+++ vue-ambassador/vue-admin/src/main.ts
@@ -5,7 +5,7 @@ import store from './store'
 import vuetify from './plugins/vuetify';
 import axios from "axios";
 
-axios.defaults.baseURL = 'http://localhost:8000/api/admin/';
+axios.defaults.baseURL = process.env.NODE_ENV === "prod" ?'http://localhost:8000/api/admin/': "heyheyhey";
 axios.defaults.withCredentials = true;
 
 Vue.config.productionTip = false
